1ab901fd0c73558edeec3354362246168580ec4a
ayaali333 <1aya.dn.ali@gmail.com>
2022-10-18 00:39:53 +0300

    finish

---
+++ src/components/ArticleList.js
@@ -0,0 +1,25 @@
+import Ract from "react"
+import Article from "./Article";
+
+
+
+const ArticleList = ({posts}) => {
+    const  articles = posts.map((post) => (
+        <Article
+          key={post.id}
+          title={post.title}
+          date={post.date}
+          preview={post.preview}
+          minutes={post.minutes}
+        />
+      ));
+    return(
+        <div>
+            <main>
+                {articles}
+
+            </main>
+        </div>
+    )
+}
+export default  ArticleList;
